a4f941e17d112b4715e18fd0d328fe96b0020499
Shaan Jassal <shaanjassal101@gmail.com>
2022-09-06 19:05:29 +0100

    Same usability but using a thread
    Signed-off-by: Shaan Jassal <shaanjassal101@gmail.com>

--- tcpserver.c
+++ tcpserver.c
@@ -4,15 +4,44 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <pthread.h>
+
+struct thread_data{
+	int socfd;
+	struct addrinfo *server;
+	struct sockaddr clientaddr;
+	socklen_t client_len;
+	int size;
+	char buffer[1024];
+};
+
+void* threadfunction(void *arg){
+	struct thread_data *my_data;
+	my_data = (struct thread_data *) arg;
+	getnameinfo(&(my_data->clientaddr),  // this sets up buffer
+				my_data->client_len,
+				my_data->buffer,
+				my_data->size, 0, 0, NI_NUMERICHOST);
+
+	send(my_data->socfd, my_data->buffer, strlen(my_data->buffer),0); /* send the address */
+	close(my_data->socfd); /* clean-up and close */
+	freeaddrinfo(my_data->server);
+	close(my_data->socfd);
+	//pthread_join(thread1, NULL);
+	return(0);
+}
 
 int main()
 {
-	struct addrinfo hints,*server;
-	int r,sockfd,clientfd;
+	struct addrinfo hints, *server;
+	int r, sockfd, clientfd;
 	struct sockaddr clientaddr;
 	socklen_t client_len;
 	const int size = 1024;
 	char buffer[size];
+
+	pthread_t thread1;
+	struct thread_data s1;
 
 	/* configure the server to use the localhost, port 8080, TCP */
 	memset( &hints, 0, sizeof(struct addrinfo) );	/* use memset_s() */
@@ -36,7 +65,7 @@ int main()
 
 	/* bind the server to a socket */
 	r = bind(sockfd,server->ai_addr,server->ai_addrlen);
-	if( r==-1 )
+	if(r == -1)
 	{
 		perror("failed");
 		exit(1);
@@ -45,7 +74,7 @@ int main()
 	/* listen for incoming connections */
 	printf("The TCP Server is listening...\n");
 	r = listen(sockfd,1);
-	if( r==-1 )
+	if(r ==-1)
 	{
 		perror("failed");
 		exit(1);
@@ -60,17 +89,19 @@ int main()
 		exit(1);
 	}
 
+	s1.socfd = clientfd;
+	s1.server = server;
+	s1.clientaddr = clientaddr;
+	s1.client_len = client_len;
+	s1.size = 1024;
+
+	pthread_create(&thread1, NULL, threadfunction, &s1);
+	pthread_join(thread1, NULL);
+	return 0;
+}
+
+
 	/* client is now connected */
 	/* no need to receive anything; just send them their address */
-	getnameinfo(&clientaddr,client_len,buffer,size,0,0,NI_NUMERICHOST);
-		
-	/* send the address */
-	send(clientfd,buffer,strlen(buffer),0);
-	
-	/* clean-up and close */
-	close(clientfd);
-	freeaddrinfo(server);
-	close(sockfd);
 
-	return(0);
-}
+
