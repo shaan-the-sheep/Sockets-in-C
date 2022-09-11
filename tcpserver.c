568a3e58e439a9691d5bd98d1481bef1a910e391
Jaswinder Jassal <jaswinder.jassal@cirrus.com>
2022-09-11 05:09:39 +0100

    Use a thread pool to manage client threads
    Signed-off-by: Jaswinder Jassal <jaswinder.jassal@cirrus.com>

--- tcpserver.c
+++ tcpserver.c
@@ -6,32 +6,64 @@
 #include <netdb.h>
 #include <pthread.h>
 
-struct thread_data{
+#define PORT ("8080")
+#define MAX_TXT_SZ 50
+
+struct socket_data {  // Used to be thead_data
 	int clientfd;
-	//struct addrinfo *server;
 	struct sockaddr clientaddr;
 	socklen_t client_len;
 	int size;
-	char buffer[1024];
-	char input[1024];
-	char shit[5];
+	char input[MAX_TXT_SZ];
 };
+
+
+struct thread_pool{
+	pthread_t thread_id;
+	struct socket_data sock_data;
+};
+
+#define THREAD_POOL_SZ 10
+struct thread_pool pool[THREAD_POOL_SZ];
+
+// Creates a new thread with the passed in fn from the next available in the
+// thread pool.
+// Returns the index of the new thread pool object, else -1
+int assign_pool_thread(int next_free_thr_index,
+					   void *(*thr_fn)(void* arg),
+					   struct socket_data sock_data)
+{
+	//printf("In assign_pool_thread. next_free_thr_index: %d\n", next_free_thr_index);
+	
+	if(next_free_thr_index >= THREAD_POOL_SZ)
+		return -1;
+	
+	// Update pool thread's socket data
+	pool[next_free_thr_index].sock_data = sock_data;
+	// Need to zero out the txt array otherwise it contains garbage.
+ 	memset(pool[next_free_thr_index].sock_data.input, 0, MAX_TXT_SZ);
+
+	int ret = pthread_create(&(pool[next_free_thr_index].thread_id),
+							 NULL, thr_fn, &(pool[next_free_thr_index].sock_data));
+	if (ret != 0)
+		return -1;
+
+	printf("New client: %d\n", pool[next_free_thr_index].sock_data.clientfd);
+	return next_free_thr_index;
+}
 
 void* threadfunction(void *arg){
 	int retval;
-	struct thread_data *my_data;
-	my_data = (struct thread_data *) arg;
-	printf("%s\n", my_data->shit);  // debug
+	struct socket_data *my_data;
+	my_data = (struct socket_data *) arg;
 	for(;;){
 		printf("Waiting for client data\n");
-		int r = recvfrom(
-			my_data->clientfd,
-			my_data->input,
-			my_data->size,
-			0,
-			&(my_data->clientaddr),
-			&(my_data->client_len)
-		);		
+		int r = recvfrom(my_data->clientfd,
+						my_data->input,
+						my_data->size,
+						0,
+						&(my_data->clientaddr),
+						&(my_data->client_len));		
 		if(r == -1)
 		{
 			perror("failed");
@@ -42,16 +74,11 @@ void* threadfunction(void *arg){
 			pthread_exit(&retval);
 			printf("omg\n");
 		}
-		printf("%s\n", my_data->input); 
+		printf("clientfd %d: %s\n", my_data->clientfd, my_data->input); 
+		// Clear out the text otherwise it hangs around when we print the next
+		// message we receive
+ 		memset(my_data->input, 0, MAX_TXT_SZ);
 	}
-	/* getnameinfo(&(my_data->clientaddr),  // this sets up buffer
-				my_data->client_len,
-				my_data->buffer,
-				my_data->size, 0, 0, NI_NUMERICHOST);
-
-	send(my_data->socfd, my_data->buffer, strlen(my_data->buffer),0);  send the address 
-	close(my_data->socfd);  clean-up and close */
-	//close(my_data->socfd);
 	return(0);
 	
 }
@@ -62,23 +89,16 @@ int main()
 	int r, sockfd, clientfd;
 	struct sockaddr clientaddr;
 	socklen_t client_len;
-	const int size = 1024;
-	//char buffer[size];
-	//char input[size];
 
-	pthread_t thread1;
-	pthread_t thread2;
-	struct thread_data s1;
-	strncpy(s1.shit, "aaa", 3);
-	struct thread_data s2;
-	strncpy(s2.shit, "bbb", 3);
+	// Thread pool of 10 client threads
+	static int next_free_thr_indx = 0;
 
-	/* configure the server to use the localhost, port 8080, TCP */
+	/* configure the server to use the localhost, port PORT, TCP */
 	memset( &hints, 0, sizeof(struct addrinfo) );	/* use memset_s() */
 	hints.ai_family = AF_INET;			/* IPv4 connection */
 	hints.ai_socktype = SOCK_STREAM;	/* TCP, streaming */
 	hints.ai_flags = AI_PASSIVE;		/* listen to any interface */
-	r = getaddrinfo( 0, "8080", &hints, &server );
+	r = getaddrinfo( 0, PORT, &hints, &server );
 	if( r!= 0 )
 	{
 		perror("failed");
@@ -110,9 +130,6 @@ int main()
 		exit(1);
 	}
 
-	// hacky thread management
-	int thr_num = 0;
-
 	/* accept a new connection */
 	client_len = sizeof(clientaddr);
 	for (;;) {
@@ -122,32 +139,27 @@ int main()
 			perror("failed");
 			exit(1);
 		}
-		if (thr_num == 0) {
-			s1.clientfd = clientfd;
-			//s1.server = server;
-			s1.clientaddr = clientaddr;
-			s1.client_len = client_len;
-			s1.size = 1024;
-			thr_num++;
-			pthread_create(&thread1, NULL, threadfunction, &s1);
+
+		struct socket_data sock_data;
+		sock_data.clientfd = clientfd;
+		sock_data.clientaddr = clientaddr;
+		sock_data.client_len = client_len;
+		sock_data.size = MAX_TXT_SZ;
+		int ret = assign_pool_thread(next_free_thr_indx, threadfunction, sock_data);
+		if (ret >= 0) {
+			next_free_thr_indx += 1;
+			//printf("Incremented next_free_thr_indx to %d\n", next_free_thr_indx);
 		}
-		if (thr_num == 1) {
-			s2.clientfd = clientfd;
-			//s2.server = server;
-			s2.clientaddr = clientaddr;
-			s2.client_len = client_len;
-			s2.size = 1024;
-			pthread_create(&thread2, NULL, threadfunction, &s2);
+		else {
+			/* Here we should really take some action to abort the execution
+			but first ensure all client threads get destroyed and join them
+			here */
 		}
-		//char input[1024]; 
 	}
 
-	pthread_join(thread1, NULL);
+	//pthread_join(thread1, NULL);
 	return 0;
 }
 
-
-	/* client is now connected */
-	/* no need to receive anything; just send them their address */
 
 
